df863478d2ec36cd568ea0e98ad24744800f85d1
monycky <monyckyfrazao@gmail.com>
2017-10-03 15:50:36 -0300

    Search tree element

--- Node.js
+++ Node.js
@@ -1,8 +1,30 @@
 class Node {
     constructor(key, parent = null, value, rightChild = null, leftChild = null) {
-       this.key = key;
-         this.parent = parent;
-         this.value = value;
-         this.rightChild = rightChild;
-         this.leftChild = leftChild;
-   }
+        this.key = key;
+        this.parent = parent;
+        this.value = value;
+        this.rightChild = rightChild;
+        this.leftChild = leftChild;
+
+        find(key) {
+            if (this.key == key) {
+                console.log('EXISTE');
+                return this;
+            } else {
+                if (key > this.key) {
+                    if (this.hasLeftChild()) {
+                        return null
+                    } else {
+                        this.rightChild.find(key)
+                    }
+                } else {
+                    if (this.hasRightChild()) {
+                        return null
+                    } else {
+                        this.leftChild.find(key)
+                    }
+                }
+            }
+        }
+    }
+}
