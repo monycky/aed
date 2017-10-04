87d9658d89742e70abb3381ed21a73741586fab9
monycky <monyckyfrazao@gmail.com>
2017-10-04 16:06:34 -0300

    Edge for graphs

---
+++ src/graphs/Edge.js
@@ -0,0 +1,11 @@
+class Edge{
+    constructor(origin, destiny, weight = null){
+        this.origin = origin;
+        this.destiny = destiny;
+        this.weight = weight;
+      }
+    
+  }
+  
+  export default Edge;
+  
