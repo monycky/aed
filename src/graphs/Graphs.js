bcb22c20ed9846a2338c47ec4dab0363ea969d72
monycky <monyckyfrazao@gmail.com>
2017-11-14 17:13:23 -0300

    Changes on addEdge, creating adjacencyList and dsf

--- src/graphs/Graphs.js
+++ src/graphs/Graphs.js
@@ -1,25 +1,46 @@
-import Edge from './Edge';
-import Vertex from './Vertex';
+import Edge from "./Edge";
+import Vertex from "./Vertex";
 
 class Graphs {
-    constructor(vertexes = [], edges = []) {
-        this.vertexes = vertexes;
-        this.edges = edges;
+  constructor(vertexes = [], edges = []) {
+    this.vertexes = vertexes;
+    this.edges = edges;
+  }
 
-    }
+  addVertex(key) {
+    let vertex = new Vertex(key);
+    this.vertexes.push(vertex);
+    return vertex;
+  }
 
-    addVertex(key) {
-        let vertex = new Vertex(key);
-        this.vertexes.push(vertex);
-        return vertex;
-    }
+  addEdge(origin, destiny) {
+    let nedge = new Edge(origin, destiny);
+    this.edges.push(nedge);
+    origin.adjacency.push(nedge);
 
-    addEdge(origin, destiny) {
-        let edge = new Edge.push(origin, destiny);
-        this.edge.push(edge);
-        return edge;
-    }
+    let nedge2 = new Edge(origin, destiny);
+    destiny.adjacency.push(nedge2);
+    return nedge;
+  }
+  adjacencyList() {
+    this.vertexes.forEach(v => v.adjacencyList());
+  }
 
+  dfs(vertex) {
+    let v;
+    this.v = vertex;
+    v.vertex.visited = true;
+    vertex.ajacency.forEach(edge => {
+      if (edge.status == "UNEXPLORED") {
+        if (edge.destiny.viseted == false) {
+          edge.status = "DISCOVERED";
+          dfs(edge.destiny);
+        } else {
+          edge.status = "RETURN";
+        }
+      }
+    });
+  }
 }
 
 export default Graphs;
